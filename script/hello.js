55a0fdc2a75a1c32a464c8872d8a980a2055838b
Heru Kurniawan <herukurniawan1707@gmail.com>
2026-07-05 22:54:06 +0700

    add some cod and understanding how to use string and number

--- script/hello.js
+++ script/hello.js
@@ -1,1 +1,4 @@
-document.writeln("hello World");
+let weight = parseInt(prompt('entar your weight'));
+let height = parseInt(prompt('entar your height'));
+let gender = prompt('entar your gender as M or F');
+let bmiCategory;
