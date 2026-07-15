1e9830ef899404c94fa23854e57c2e9586f2f541
Heru Kurniawan <herukurniawan1707@gmail.com>
2026-07-15 23:12:23 +0700

    add input text conect to js

--- script/hello.js
+++ script/hello.js
@@ -1,9 +1,8 @@
 
+let username;
 
-let fullName = 'Heru kurniawan';
-let age = 24;
-let isStudent = false;
-
-document.getElementById("p1").textContent = `add text like this and my name is ${fullName}`;
-document.getElementById("p2").textContent = `and i'm ${age} years old`;
-document.getElementById("p3").textContent = `enroled: ${isStudent}`;
+document.getElementById("my submit").onclick = function(){
+  username = document.getElementById("my text").value;
+  document.getElementById("my h1").textContent = `hello ${username}`
+  console.log(username)
+}
