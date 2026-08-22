29ca1b1d7ddd225d07cf5a7739c29be9e22ba068
Heru Kurniawan <herukurniawan1707@gmail.com>
2026-08-22 22:23:23 +0700

    learn about function and global scope

--- count.js
+++ count.js
@@ -10,3 +10,17 @@ function increment() {
   console.log("the button was clicked")
 }
  
+function sixSeven() {
+  console.log(42)
+}
+sixSeven()
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function lap() {
+  let totalLap = lap1 + lap2 + lap3
+  console.log(totalLap)
+}
+lap()
